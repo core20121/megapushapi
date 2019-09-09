b64e48e30cd75e4e74fc065ffbbce365ee91d2d1
core20121 <steflabs2012@gmail.com>
2019-09-09 17:43:04 +0300

    'asd'

---
+++ redmegapush/orig/api.php
@@ -0,0 +1,74 @@
+
+<?php
+class MegapushApi
+{
+	private $auth;
+	public  $token;
+	private $api_token;
+	private $curl;
+	private $default_url = 'https://megapu.sh/megaApi/campaigns/';
+
+	const DEVICE_MOBILE  = '1';
+	const DEVICE_DESCTOP = '2';
+
+	const OPSYS_MOBI = ["Android4","Android5","Android6","Android6","Android7","Android8","Android9","Android10"];
+	const OPSYS_DESC = ["Windows7","Windows8","Windows10"];
+
+	const ACTION_GET_ALL = 'getAll';
+	const ACTION_GET_BY_ID = 'getById';
+	const ACTION_CREATE = 'create';
+	const ACTION_DELETE = 'delete';
+	const ACTION_UPDATE = 'update';
+	const ACTION_START = 'start';
+	const ACTION_STOP = 'stop';
+
+	public function __construct($auth, $token, $api_key) {
+		$this->auth = $auth;
+		$this->token = $token;
+		$this->api_token = $api_key;
+	}
+
+	private function getHeaders(): array {
+		return [
+			'Authorization: ' . $this->auth,
+			'Token: ' . $this->token,
+			'ApiKey: ' . $this->api_token
+		];
+	}
+
+	private function initCurl(array $params, $action) {
+		$params['a'] = $action;
+		$this->curl = curl_init('https://megapu.sh/megaApi/campaigns/' . '?' .  http_build_query($params));
+
+		curl_setopt($this->curl , CURLOPT_HTTPHEADER, $this->getHeaders());
+		curl_setopt($this->curl, CURLOPT_POST, 1);
+		curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($params));
+
+		return curl_exec($this->curl);
+	}
+
+	public function getAll(array $params = []) {
+		return $this->initCurl($params, static::ACTION_GET_ALL);
+	}
+	public function getById(array $params = []) {
+		return $this->initCurl($params, static::ACTION_GET_BY_ID);
+	}
+	public function createCampagin(array $params = []) {
+		return $this->initCurl($params, static::ACTION_CREATE);
+	}
+	public function deleteCampagin(array $params = []) {
+		return $this->initCurl($params, static::ACTION_DELETE);
+	}
+	public function updateCampagin(array $params = []) {
+		return $this->initCurl($params, static::ACTION_UPDATE);
+	}
+	public function stopCampagin(array $params = []) {
+		return $this->initCurl($params, static::ACTION_STOP);
+	}
+	public function startCampagin(array $params = []) {
+		return $this->initCurl($params, static::ACTION_STOP);
+	}
+}
+
+?>
+
---
+++ redmegapush/orig/index.php
@@ -0,0 +1,71 @@
+<?php 
+include 'api.php';
+
+$auth  = 'MegaApi';
+$token = 'WjFKOU51ZG5nQzN1NEdvUHQ3';
+$api_key = '60b24b74';
+$api = new MegapushApi($auth, $token, $api_key);
+
+$titles = [
+	"Israel car sharing",
+	"cars for israel citizens",
+	"Car Sharing",
+	"CAR SHARING",
+];
+
+$description = [
+	"Free minutes car sharing",
+	"Free 15 minutes from partners",
+	"30 min free for registration",
+	"30 min free on praticipation",
+	"sign up with our partners and get a bonus",
+	"how to get free minutes of car rental?",
+	"sign up and get a free 15 minutes",
+	"Free minutes",
+	"Free Kilometers",
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//пустые значения заполенны после того как
+//в сапорте сказали "все являются обязательными"
+$create_params = [
+	'c_name'  => 'is_qweqwe',
+	'c_title' => 'FreeWebHosting',
+	'c_desc' => 'Freehostingwithoutimitsonthenumberofsites',
+	'c_url' => 'http://ststudyabroad.by/hosting/hosting/',
+	'c_country' => 'IL',
+	'c_tg' => '2', //device
+	'op_system' => '["Windows7","Windows8","Windows10"]',
+	'start_time' => '',
+	'stop_time' => '',
+	'c_image' =>  'http://bestac.kz/isa/img/111111.jpg',
+	'c_icon' => 'http://bestac.kz/isa/img/omg2.jpg',
+	'c_limit' => '200',
+	'mob_carrier' => '',
+	'cpc' => '1',
+	'blocked_list' => '[]',
+	'feeds' => '["feed1","feed10","feed36"]',
+	'link_type' => '',
+];
+
+$get_by_id = [
+	'camp_id' => '844273'
+];
+
+//$api->getAll();
+$api->getById($get_by_id);
+//$api->createCampagin($create_params);
